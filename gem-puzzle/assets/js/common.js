b82351004fd19ec7f7889b0f49dcf5922bfb7b33
Aliaksandr Kulik <inter_net@tut.by>
2020-11-04 20:21:41 +0300

    feat: add object

--- gem-puzzle/assets/js/common.js
+++ gem-puzzle/assets/js/common.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
-	
-});
+const Puzzle = {
+  elements: {
+    puzzle: null,
+    grid: null,
+    gems: []
+  },
+
+   properties: {
+    unknown1: false,
+    unknown2: false
+  },
+
+   init(lang) {
+    
+    this.elements.puzzle = document.createElement("div");
+    this.elements.grid = document.createElement("div");
+    
+    this.elements.puzzle.classList.add("puzzle");
+    this.elements.grid.classList.add("grid");
+   
+    this.elements.grid.appendChild(this.createGems());
+    this.elements.gems = this.elements.grid.querySelectorAll(".gem");
+    
+    this.elements.puzzle.appendChild(this.elements.grid);
+    document.body.appendChild(this.elements.puzzle);
+    buttons = Array.from(this.elements.buttons);
+  },
